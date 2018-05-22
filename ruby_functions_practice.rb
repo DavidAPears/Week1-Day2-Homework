3bfd97e61068c16571e7ba9d7cc570812f3a664c
David Pears <davidapears@gmail.com>
2018-05-22 19:30:24 +0100

    Trying to reassign the git

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,74 @@
+def return_10()
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(string1, string2)
+  return string1.to_i + string2.to_i
+end
+
+def number_to_full_month_name(number)
+  month_name = case number
+    when 1
+      "January"
+    when 2
+      "February"
+    when 3
+      "March"
+    when 4
+      "April"
+    when 5
+      "May"
+    when 6
+      "June"
+    when 7
+      "July"
+    when 8
+      "August"
+    when 9
+      "September"
+    when 10
+      "October"
+    when 11
+      "November"
+    when 12
+      "December"
+  end
+    return month_name
+end
+
+
+def number_to_short_month_name(number)
+   case number
+    when 1
+      "January".slice(0, 2)
+    when 4
+      "April".slice(0, 2)
+    when 10
+      "October".slice(0, 2)
+    end
+      return
+end
